63d7d401828a2f664a206c700f82f84b1c10c9c7
Angelo Spampinato <spampinatodesign@gmail.com>
2019-03-17 14:44:25 -0400

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
 
 // Write your code here
+const var1 = 31;
+const var2 = 2;
+
+let multiply = var1 * var2;
+let random = Math.floor(Math.random() * 10) + 1;
+let mod = 10 % 6;
+let max = Math.max(20, 12, 1, 2, 3, 4, 5);
